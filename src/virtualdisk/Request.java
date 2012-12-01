f081dde2a7e3896b5c82817c811ed0f20d6f96c9
apalepu23 <ross.michael22@gmail.com>
2012-12-01 03:57:53 -0500

    Added Request file

---
+++ src/virtualdisk/Request.java
@@ -0,0 +1,16 @@
+package virtualdisk;
+
+import common.Constants.DiskOperationType;
+
+import dblockcache.DBuffer;
+
+public class Request 
+{
+	private DBuffer myBuf; 
+	private DiskOperationType myOperation; 
+	public Request(DBuffer buf, DiskOperationType operation)
+	{
+		myBuf = buf; 
+		myOperation = operation; 
+	}
+}
