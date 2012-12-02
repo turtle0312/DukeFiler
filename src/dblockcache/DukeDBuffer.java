0e9fba01e2bc16fea202c5f564b85e58bb0ae56e
ParkerKuivila@gmail.com <ParkerKuivila@gmail.com>
2012-12-02 18:25:22 -0500

    read/write() now done in Buffer

--- src/dblockcache/DukeDBuffer.java
+++ src/dblockcache/DukeDBuffer.java
@@ -15,7 +15,7 @@ public class DukeDBuffer extends DBuffer
 	// Is the clean? / Has it NOT been modified
 	private boolean isClean = true;
 
-
+	//Parker is so smart and good looking
 	public DukeDBuffer(int blockID, int bufferSize) {
 		myBlockID = blockID;
 		myData = new byte[bufferSize];
@@ -96,14 +96,36 @@ public class DukeDBuffer extends DBuffer
 
 	@Override
 	public int read(byte[] buffer, int startOffset, int count) {
-		// TODO Auto-generated method stub
-		return 0;
+		// Read in data from block, start write to buffer at location: offset
+		holdRead();
+		try {
+			for (int i=0; i<count; i++) { //iterate over 
+				buffer[startOffset + 1] = myData[i];
+			}
+		catch (Exception e)
+			{
+			releaseRead(); retun -1;
+			}
+		releaseRead();
+		return 1; //what should we return?
+		}
 	}
 
 	@Override
 	public int write(byte[] buffer, int startOffset, int count) {
-		// TODO Auto-generated method stub
-		return 0;
+		// Write in data to block, start write to block from buffer location: offset
+		holdWrite();
+		try {
+			for (int i=0; i<count; i++) { //iterate over 
+				myData[i] = buffer[startOffset + 1]
+			}
+		catch (Exception e)
+			{
+			releaseWrite(); retun -1;
+			}
+		releaseWrite();
+		return 1; //what should we return?
+		}
 	}
 
 	@Override
