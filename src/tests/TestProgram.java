17f39174bb35a64bfbc93226706c96c34c6c0463
Mike <mross@mross-900X3C-900X4C-900X4D.(none)>
2012-12-04 13:46:38 -0500

    updated test program

--- src/tests/TestProgram.java
+++ src/tests/TestProgram.java
@@ -1,19 +1,22 @@
 package tests;
 
+import common.DFileID;
+
 import dfs.DFS;
 import dfs.DukeDFS;
 
 public class TestProgram {
 	public static void main(String[] args){
 		DFS dukeDFS = new DukeDFS();
-		System.out.println("Create DFile: " + 	dukeDFS.createDFile().getDFileID());
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("Create DFile: " + 	dukeDFS.createDFile().getDFileID());
-		System.out.println("Create DFile: " + 	dukeDFS.createDFile().getDFileID());
-	}	
+		int fileID = dukeDFS.createDFile().getDFileID();
+		System.out.println("Create DFile: " + 	fileID);
+		byte[] testMessage = new String("Test!").getBytes();
+		int bytesWritten = dukeDFS.write(new DFileID(fileID), testMessage, 0, testMessage.length);
+		System.out.println("Bytes written: " + bytesWritten);
+		byte[] readBuffer = new byte[testMessage.length];
+		int bytesRead = dukeDFS.read(new DFileID(fileID), readBuffer, 0, testMessage.length);
+		String test = new String(readBuffer);
+		System.out.println("Test read: " + test);
+		System.out.println("Bytes read: " + bytesRead);
+	}		
 }
